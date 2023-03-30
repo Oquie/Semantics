619888a6fc6783a620ebb30e31118f2025de33bc
oquie <stellanwachukwu@gmail.com>
2023-03-30 19:45:41 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+
+provider "aws" {
+  shared_credentials_file = "$HOME/.aws/credentials"
+  #profile                 = "default"
+  provider "aws" {
+  shared_credentials_file = "$HOME/.aws/credentials"
+  profile                 = "default"
+  region                  = "us-east-1"
+ }
+} 
